cd0dbaf329f5dd7b82a04121ddcf2ddd2c07fb46
Balveer <balveermahor88599@gmail.com>
2026-02-21 20:45:01 +0530

    create the userSchema in user.model.js

---
+++ Backend/models/user.model.js
@@ -0,0 +1,31 @@
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    credits: {
+        type: Number,
+        default: 100,
+        min: 0  
+    },
+    isCreditsAvailable: {
+        type: Boolean,
+        default: true
+    },  
+    notes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Notes",
+        default: []
+    }]
+},{timestamps: true})
+
+const User = mongoose.model("UserModel", userSchema)
+
+export default User;
--- Backend/utils/connectDb.js
+++ Backend/utils/connectDb.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MoNGODB_URI)
+        console.log("Connected to MongoDB")
     } catch (error) {
-        
+        console.error("Error connecting to MongoDB:", error)
     }
 }
+
+export default connectDb;
